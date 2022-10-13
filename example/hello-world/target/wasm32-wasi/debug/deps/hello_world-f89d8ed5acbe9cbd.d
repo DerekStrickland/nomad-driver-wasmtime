fe2931dadf669f9de87526ec730e91e9e1e87bec
Derek Strickland <derek.strickland@gmail.com>
2022-10-13 17:27:23 -0400

    remove build output

--- example/hello-world/target/wasm32-wasi/debug/deps/hello_world-f89d8ed5acbe9cbd.d
+++
@@ -1,5 +0,0 @@
-/Users/derekstrickland/code/nomad-driver-wasmtime/example/hello-world/target/wasm32-wasi/debug/deps/hello_world-f89d8ed5acbe9cbd.wasm: src/main.rs
-
-/Users/derekstrickland/code/nomad-driver-wasmtime/example/hello-world/target/wasm32-wasi/debug/deps/hello_world-f89d8ed5acbe9cbd.d: src/main.rs
-
-src/main.rs:
--- example/hello-world/target/wasm32-wasi/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/Users/derekstrickland/code/nomad-driver-wasmtime/example/hello-world/target/wasm32-wasi/debug/hello-world.wasm: /Users/derekstrickland/code/nomad-driver-wasmtime/example/hello-world/src/main.rs
